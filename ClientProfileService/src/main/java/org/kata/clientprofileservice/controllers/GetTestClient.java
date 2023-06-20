04aa8404479fd78efde5b1f2f32126ca739a7da3
SashaPerepelkin <apocaliptorus@gmail.com>
2023-06-20 19:17:11 +0300

    hanged the return type of the getestatandividual method, added dependencies for swagger and documentation for the etestlient controller

--- ClientProfileService/src/main/java/org/kata/clientprofileservice/controllers/GetTestClient.java
+++ ClientProfileService/src/main/java/org/kata/clientprofileservice/controllers/GetTestClient.java
@@ -1,7 +1,13 @@
 package org.kata.clientprofileservice.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.AllArgsConstructor;
 import org.kata.clientprofileservice.service.TestDataIndividualService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,10 +19,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class GetTestClient {
     private final TestDataIndividualService dataIndividual;
 
-
+    @Operation(summary = "Creates the number of individuals passed in the url parameters. " +
+            "Passes a message about successful creation of data to the TestUser topic.", tags = "Test data")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Successful creation of test data",
+                    content = @Content(mediaType = "text/plain",
+                                       examples = @ExampleObject(value = "Successful creation of test data, count objects: 3"))
+            )
+    })
     @GetMapping("/get")
-    public void getTestDataIndividual(@RequestParam(value = "count", defaultValue = "1") int count) {
+    public ResponseEntity<String> getTestDataIndividual(@RequestParam(value = "count", defaultValue = "1") int count) {
         dataIndividual.createIndividuals(count);
+        String message = "Successful creation of test data, count objects: " + count;
+        return ResponseEntity.ok(message);
     }
 
 }
+
