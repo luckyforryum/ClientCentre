8413d6a2eba4379c4a7862b1c13735f59cd65a87
Yurik3 <105162903+Yurik3@users.noreply.github.com>
2023-07-08 17:10:20 +0300

    1)Добавлен модуль сервера Eureka. (#19)
    2)Добавлены в pom микросервисов зависимости для клиента Eureka
    3) В стартовые файлы добавлены аннотации для клиента
    4) В пропертях микросервисов добавлены настройки для их регистрации на Eurika сервере

--- ClientProfileFacade/src/main/java/com/kata/clientprofileauthentication/ClientProfileFacadeApplication.java
+++ ClientProfileFacade/src/main/java/com/kata/clientprofileauthentication/ClientProfileFacadeApplication.java
@@ -6,9 +6,10 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-
+@EnableDiscoveryClient
 @OpenAPIDefinition(
         info = @Info(
                 title = "Masking microservice",
--- ClientProfileNotification/src/main/java/com/kata/clientprofilenotification/ClientProfileNotificationApplication.java
+++ ClientProfileNotification/src/main/java/com/kata/clientprofilenotification/ClientProfileNotificationApplication.java
@@ -2,8 +2,10 @@ package com.kata.clientprofilenotification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ClientProfileNotificationApplication {
 
     public static void main(String[] args) {
